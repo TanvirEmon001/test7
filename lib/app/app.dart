3ae1fd36d6c64df09589db617b8cc86ba257a6a3
tanviremon001 <tanvirofficial121@gmail.com>
2026-02-22 00:21:09 +0600

    Folder restrucutred

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:test_seven/home_screen.dart';
+import 'package:test_seven/screens/home_screen.dart';
 class App extends StatelessWidget {
   const App({super.key});
 
